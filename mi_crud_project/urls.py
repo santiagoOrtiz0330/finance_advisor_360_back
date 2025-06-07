8999a6935b9d26637ef6d96bcc78f5ad45ec251c
santiagoOrtiz0330 <36429742+santiagoOrtiz0330@users.noreply.github.com>
2025-06-07 14:28:54 -0500

    url

--- mi_crud_project/urls.py
+++ mi_crud_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('api/', include('clientes.urls')),
+     path('app/api/', include('clientes.urls')),
 ]
