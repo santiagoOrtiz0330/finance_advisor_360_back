471ae0d4ef6b514ce55c628a238ad771cc5f6955
santiagoOrtiz0330 <36429742+santiagoOrtiz0330@users.noreply.github.com>
2025-06-07 15:24:36 -0500

    cambiar url

--- mi_crud_project/urls.py
+++ mi_crud_project/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('app/admin/', admin.site.urls),
-     path('app/api/', include('clientes.urls')),
+    path('admin/', admin.site.urls),
+     path('api/', include('clientes.urls')),
 ]
